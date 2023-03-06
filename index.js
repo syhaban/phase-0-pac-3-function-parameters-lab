67e57d6f310ec6d1852a39243f1b59312ed64706
Syha Ban <syhaban@hotmail.com>
2023-03-05 23:58:34 -0500

    Adds index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+  }
+  function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+  }
+  function sayHelloTo(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
+  }
